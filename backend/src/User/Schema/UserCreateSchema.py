005572eee5bfdc8048727d5466e67013d3ee3d83
gustavoalmeidam7 <gustavoalmeidam7@gmail.com>
2025-08-22 11:38:31 -0300

    DOCS/ Atualizado schemas para exemplos de uso práticos

--- backend/src/User/Schema/UserCreateSchema.py
+++ backend/src/User/Schema/UserCreateSchema.py
@@ -7,12 +7,12 @@ from src.Model.User import User
 from src.User.Service.Utils import unmask_number, validate_birthday
 
 class UserCreateSchema(BaseModel):
-    username:      Annotated[str, Field(max_length=35)]
-    cpf:           Annotated[str, Field(max_length=11), AfterValidator(unmask_number)]
-    birthday:      Annotated[date, Field(), AfterValidator(validate_birthday)]
-    email:         Annotated[EmailStr, Field(data="email", max_length=45)]
-    phone_number : Annotated[str, Field(data="phone_number", max_length=14), AfterValidator(unmask_number)]
-    password:      Annotated[str, Field()]
+    username:      Annotated[str, Field(max_length=35)] = "Ronaldo de Assis Moreira"
+    cpf:           Annotated[str, Field(max_length=11), AfterValidator(unmask_number)] = "12345678915"
+    birthday:      Annotated[date, Field(), AfterValidator(validate_birthday)] = date(1980, 3, 21)
+    email:         Annotated[EmailStr, Field(data="email", max_length=45)] = "ronaldo@mail.com"
+    phone_number : Annotated[str, Field(data="phone_number", max_length=14), AfterValidator(unmask_number)] = "51991234567"
+    password:      Annotated[str, Field()] = "SenhaSegura123"
 
     def to_model(self) -> User:
         return User(
