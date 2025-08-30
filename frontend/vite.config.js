c75c89917e930cec3e49ab751999ba4de6a1b511
gustavoalmeidam7 <gustavoalmeidam7@gmail.com>
2025-08-30 15:56:34 -0300

    FEAT/ Teste de deploy no github pages usando github workflow

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/tcc-sga/",
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
