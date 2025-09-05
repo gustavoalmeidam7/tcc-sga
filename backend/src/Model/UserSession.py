9dac094202b00f0a321bd47ac64738337f2a5a7d
gustavoalmeidam7 <gustavoalmeidam7@gmail.com>
2025-09-05 11:22:28 -0300

    FIX/ Trocado valid until e timestamp

--- backend/src/Model/UserSession.py
+++ backend/src/Model/UserSession.py
@@ -10,8 +10,8 @@ class Session(BaseModel):
     id = UUIDField(primary_key=True, default=uuid.uuid4)
     user = ForeignKeyField(User, backref="sessions")
     ip = CharField(max_length=39)
-    validUntil = DateTimeField(default=datetime.now(timezone.utc))
-    timestamp = DateTimeField(default=datetime.now(timezone.utc) + timedelta(minutes=30))
+    validUntil = DateTimeField(default=datetime.now(timezone.utc) + timedelta(minutes=30))
+    timestamp = DateTimeField(default=datetime.now(timezone.utc))
 
     class Meta:
         table_name = "session"
