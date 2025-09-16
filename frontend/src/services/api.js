9cecc43c6ed3ba4ce479f18306e47c45fc9b632f
Maicon.Rodrigues <165661619+MaiconS2@users.noreply.github.com>
2025-09-16 14:04:18 -0300

    FIX/ Alteração na url da api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getAuthToken } from './tokenStore';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000';
+const API_URL = import.meta.env.VITE_API_URL;
 
 const API = axios.create({
   baseURL: API_URL,
