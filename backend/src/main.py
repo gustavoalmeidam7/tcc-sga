1874ce373e0dcdf8a331952f52632997b8bbc3db
gustavoalmeidam7 <gustavoalmeidam7@gmail.com>
2025-09-17 08:21:47 -0300

    FIX/ Corrigido uso do lifespan para inicialização da api

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,14 +1,38 @@
-from fastapi import Request
+from fastapi import Request, FastAPI
 from fastapi.responses import JSONResponse
-from src.Error.ErrorClass import ErrorClass
-from src.Model import User, Driver, Travel, UserSession
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 from fastapi.middleware.cors import CORSMiddleware
+
+from contextlib import asynccontextmanager
+
 from src.Utils.env import get_env_var
 
-from src.Controller import app
+from src.Controller.UserController import USER_ROUTER
+from src.Controller.AuthController import AUTH_ROUTER
+
+from src.Error.ErrorClass import ErrorClass
+
+from src.Model import User, Driver, Travel, UserSession
+
+from src.Utils.env import get_env_var
 
 from src.DB import db
+
+env = get_env_var("environment", "DEV")
+
+isDebug = (env == "DEV")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    db.connect()
+    db.create_tables([User.User, Driver.Driver, Travel.Travel, UserSession.Session])
+    yield
+    db.close()
+
+app = FastAPI(debug=isDebug, title="Gerenciamento de ambulância API", description="Api para gerenciamento de ambulâncias - TCC", version="1.0.0", root_path="/api", lifespan=lifespan)
+
+app.include_router(USER_ROUTER)
+app.include_router(AUTH_ROUTER)
 
 app.add_middleware(
     CORSMiddleware,
@@ -27,8 +51,3 @@ def error_handler(request: Request, error: ErrorClass):
         status_code=error.statusCode,
         content={"Erros": error.errors}
     )
-
-@app.on_event("startup")
-def main():
-    db.connect()
-    db.create_tables([User.User, Driver.Driver, Travel.Travel, UserSession.Session])
