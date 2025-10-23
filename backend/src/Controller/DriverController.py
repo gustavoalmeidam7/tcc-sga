15a8e1890c96be175922bfed643dddb54d5e76be
gustavoalmeidam7 <gustavoalmeidam7@gmail.com>
2025-10-22 21:32:30 -0300

    FIX/ Alterado definicies em controller de def para async def

--- backend/src/Controller/DriverController.py
+++ backend/src/Controller/DriverController.py
@@ -17,16 +17,16 @@ DriverRouter = APIRouter(
 )
 
 @DriverRouter.post("/travel/{travel}")
-def assing_driver_to_travel(driver: GET_AUTENTHICATED_DRIVER, travel: UUID) -> TravelResponseSchema:
+async def assing_driver_to_travel(driver: GET_AUTENTHICATED_DRIVER, travel: UUID) -> TravelResponseSchema:
     """ Assina o motorista e a respectiva ambulância a uma viagem """
     return DriverService.assign_driver_travel(driver, travel)
 
 @DriverRouter.get("/travels/")
-def get_assigned_travels(driver: GET_AUTENTHICATED_DRIVER) -> TravelListResponseSchema:
+async def get_assigned_travels(driver: GET_AUTENTHICATED_DRIVER) -> TravelListResponseSchema:
     """ Encontra todas viagens que um motorista está escrito """
     return DriverService.find_assigned_travels(driver)
 
 @DriverRouter.get("/")
-def get_driver_info(driver: GET_AUTENTHICATED_DRIVER) -> DriverResponseSchema:
+async def get_driver_info(driver: GET_AUTENTHICATED_DRIVER) -> DriverResponseSchema:
     """ Encontra as informações do motorista """
     return DriverService.get_driver_by_user(driver)
--- backend/src/Schema/User/UserUpdateResponseSchema.py
+++ backend/src/Schema/User/UserUpdateResponseSchema.py
@@ -1,13 +1,11 @@
 from pydantic import Field, EmailStr
 from src.Schema.BaseModel import BaseModel
 
-from src.Schema.User.UserRoleEnum import UserRole
-
 from typing import Annotated
 from uuid import uuid4, UUID
 
 class UserUpdateResponseSchema(BaseModel):
-    id        : Annotated[UUID,     Field(example=uuid4)]
+    id        : Annotated[UUID,     Field(example=uuid4())]
     email     : Annotated[EmailStr, Field(example="ronaldo@mail.com", max_length=45)]
     nome      : Annotated[str,      Field(example="Ronaldo de Assis Moreira", max_length=50)]
     telefone  : Annotated[str,      Field(example="51991234567", max_length=12)]
